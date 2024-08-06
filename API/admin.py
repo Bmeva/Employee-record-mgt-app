37cfae0500fce6473722f316dba84f2f1506031e
Bmeva <evansboma@gmail.com>
2024-08-06 01:55:35 +0100

    pascal

--- API/admin.py
+++ API/admin.py
@@ -1,3 +1,24 @@
+
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import WorkProject
+
+
+#from authentication.models import User
+
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display =('project_name', 'project_owner', 'start_date', 'end_date', 'status', 'created_at', 'modified_at')
+    list_editable = ('project_name', 'project_owner')
+    ordering= (-id)
+    #readonly_fields = ('email', 'password',) to make fields uneditable
+   
+    
+  
+    
+
+admin.site.register(WorkProject, ProjectAdmin)
+
+
--- API/models.py
+++ API/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class WorkProject(models.Model):
+    project_name = models.CharField(unique=True, max_length=120)
+    project_owner = models.CharField(unique=True, max_length=120)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    comments = models.CharField(blank=True, null=True, max_length=600)
+    status = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.Project_name}  {self.id}"
